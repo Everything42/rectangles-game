86b3d14bb26ac2bc64048476e8c65c02567f2ce6
St Peters <stpeters@Sts-MacBook-Air.local>
2023-03-04 12:05:17 +0100

    Add new file

---
+++ rectangle_pygame.py
@@ -0,0 +1,73 @@
+import pygame
+from random import randint
+from sys import exit
+
+# initialize pygame
+pygame.init()
+clock = pygame.time.Clock()
+
+# create counter
+time_counter = 0
+timer_event = pygame.USEREVENT + 1
+pygame.time.set_timer(timer_event, 1000)
+
+# initialize display
+displayX = 1440
+displayY = 780
+screen = pygame.display.set_mode((displayX, displayY))
+pygame.display.set_caption("Rectangle game")
+
+
+# create class that inherits from Surfaces
+class Rectangles(pygame.Surface):
+    def __init__(self, X, Y, dimX, dimY, VelX, VelY, color):
+        super().__init__((dimX, dimY))
+        self.X = X
+        self.Y = Y
+        self.dimX = dimX
+        self.dimY = dimY
+        self.VelX = VelX
+        self.VelY = VelY
+        self.color = color
+
+    def move(self):
+        if self.X + self.dimX > displayX: self.VelX = -1 * randint(1, 4)
+        if self.Y + self.dimY > displayY: self.VelY = -1 * randint(1, 4)
+        if self.X < 0: self.VelX = randint(1, 4)
+        if self.Y < 0: self.VelY = randint(1, 4)
+        self.X += self.VelX
+        self.Y += self.VelY
+
+    def draw(self):
+        self.fill(self.color)
+        screen.blit(self, (self.X, self.Y))
+
+    def check_collision(self, other):
+        rect1 = pygame.Rect(self.X, self.Y, self.dimX, self.dimY)
+        rect2 = pygame.Rect(other.X, other.Y, other.dimX, other.dimY)
+        return rect1.colliderect(rect2)
+
+
+# create a list with instances of Rectangles that have random attributes
+list_rect = []
+for i in range(400):
+    list_rect.append(Rectangles(X=randint(0, displayX), Y=randint(0, displayY), dimX=randint(20, 100), dimY=randint(20, 100), VelX=randint(1, 4), VelY=randint(1, 4), color=(randint(0, 255), randint(0, 255), randint(0, 255))))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        elif event.type == timer_event:
+            time_counter +=1
+
+    # screen.fill((0, 0, 0))
+    if time_counter < 5:
+        for element in list_rect:
+            element.draw()
+            element.move()
+            for other_element in list_rect:
+                if
+
+    pygame.display.update()
+    clock.tick(60)
